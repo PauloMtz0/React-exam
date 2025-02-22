55d0cfb3bd2547566f847ce0ad878b44ea78987d
Paulo <al222310520@gmail.com>
2025-02-22 09:28:52 -0600

    Update UsersList.jsx

--- src/components/userList/UsersList.jsx
+++ src/components/userList/UsersList.jsx
@@ -1,30 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
+import useUsers from '../hooks/useUsers'; 
 
-const UsersList = ({users}) => {
+const UsersList = () => {
+    const { users } = useUsers(); // Obtiene los usuarios del hook
+    const [search, setSearch] = useState('');
+
+    const filteredUsers = users.filter((usuario) =>
+        usuario.name.toLowerCase().includes(search.toLowerCase())
+    );
+
     return (
         <div>
             <h1>Lista de Usuarios</h1>
+            <input
+                type="text"
+                placeholder="Buscar por nombre..."
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
+
             <table>
                 <thead>
                     <tr>
-                        <th>nombre</th>
-                        <th>email</th>
+                        <th>Nombre</th>
+                        <th>Email</th>
                         <th>Apellido</th>
                     </tr>
                 </thead>
                 <tbody>
-                {users.map((usuario) => (
-                
-                <tr key={usuario.id}>
-                    <td>{usuario.name}</td>
-                    <td>{usuario.email}</td>
-                    <td>{usuario.last_name}</td>
-                </tr>   
-                ))}
+                    {filteredUsers.map((usuario) => (
+                        <tr key={usuario.id}>
+                            <td>{usuario.name}</td>
+                            <td>{usuario.email}</td>
+                            <td>{usuario.last_name}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
-        </div >
+        </div>
     );
 };
 
-export default UsersList
+export default UsersList;
